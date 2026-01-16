624111249d11b63b19dd99bba46bbc566c85e245
Muhammad ALI <rmuhammadali1993@gmail.com>
2026-01-16 19:51:57 +0500

    upload

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
 import express from "express"
 import multer from "multer"
+import path from "node:path"
 
 const app = express()
 const PORT = process.env.PORT
+const storage = multer.diskStorage({
+    destination: "uploads/",
+    filename: (req, file, cb) => {
+        const ext = path.extname(file.originalname)
+        const name = Date.now() + ext
+        cb(null, name)
+    }
+})
+const upload = multer({ storage })
 
 app.set("view engine", "ejs")
 app.set("views", "./src/views")
 
 app.use("/public", express.static("public"))
+app.use("/uploads", express.static("uploads"))
 
 app.get("/", async (req, res) => {
     res.render("home")
 })
 
 app.get("/send", async (req, res) => {
-    res.render("send")
+    if (req.query.file) {
+        res.render("send", {
+            file: true,
+            user_file: "/uploads/" + req.query.file
+        })
+    } else {
+        res.render("send", {
+            file: false
+        })
+    }
+})
+
+app.post("/send", upload.single("users_file"), async (req, res) => {
+    res.redirect(`./send?file=${req.file.filename}`)
 })
 
 app.get("/dashboard", async (req, res) => {
